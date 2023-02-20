87a0c4174293af005bad559756e0ef6afd3d9d14
Austin <aconnor731@gmail.com>
2023-02-20 17:51:50 -0500

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,33 @@ import './App.css';
 import * as books from './books';
 
 function App() {
-  const [isHighlighted, setIsHighlighted] = useState(0);
+  const [isHighlighted, setIsHighlighted] = useState(6000);
   const lines = books.politics.match(/.{1,150}/g);
-  console.log(lines)
+  console.log(isHighlighted)
 
   const highlighted = {
     backgroundColor: '#FFDF00',
     color: 'black',
   }
 
+  const handleKeyDown = (event: any) => {
+    console.log(event.key);
+
+    if (event.key === 'z') {
+      setIsHighlighted(isHighlighted - 1);
+    }
+
+    if (event.key === 'x') {
+      setIsHighlighted(isHighlighted + 1);
+    }
+  }
+
+
   return (
     <div>
-      <div className="App" onClick={() => setIsHighlighted(isHighlighted + 1)}>
-        <div style={{ width: '55%', padding: '20px 0 0 20px', textAlign: 'left', whiteSpace: 'pre-wrap', fontSize: '24px' }}>
-          {lines?.map((line: string, index: number) => <div style={index === isHighlighted ? highlighted : { backgroundColor: 'transparent'}}>{line}<br /></div>)}
+      <div tabIndex={0} className="App" onKeyDown={(e) => handleKeyDown(e)}>
+        <div style={{ width: '65%', padding: '20px 0 0 20px', textAlign: 'left', whiteSpace: 'pre-wrap', fontSize: '24px' }}>
+          {lines?.map((line: string, index: number) => <div key={index} style={index === isHighlighted ? highlighted : { backgroundColor: 'transparent'}}>{line}<br /></div>)}
         </div>
       </div>
     </div>
